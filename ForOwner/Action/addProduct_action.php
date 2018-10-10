12209aeb68dcc10c75d40264fcf6f6e77471dc2c
panatda.chaipayon <panatda.chaipayon@clbs.co.th>
2018-10-11 01:08:00 +0700

    fix bug upload img

--- ForOwner/Action/addProduct_action.php
+++ ForOwner/Action/addProduct_action.php
@@ -10,21 +10,23 @@ $color=$_POST['color'];
 
 include ('../../dbConnect.php');
 
-if($_FILES['filesToUpload']['size'] !== 0) {
+//if($_FILES['filesToUpload']['size'] !== 0) {
 //$filesToUpload=$_POST['filesToUpload'];
     $sql="INSERT INTO product (pdID,name,description,price,cost,dateCreate,color)VALUES('$pdID','$name','$description','$price','$cost','$dateCreate','$color')";//คำสั่งเพิ่มข้อมูล
     $sql_query=mysqli_query($con,$sql);
 
     for ($i = 0; $i < count($_FILES['filesToUpload']['name']); $i++) {
-        $image = addslashes(file_get_contents($_FILES['filesToUpload']['tmp_name'][$i]));
-        $sql2 = "INSERT INTO image (imgID,img,pdID)VALUES('','$image','$pdID')";//คำสั่งเพิ่มข้อมูล
-        $sql_query2 = mysqli_query($con, $sql2);
+        if($_FILES['filesToUpload']['size'][$i] !== 0) {
+            $image = addslashes(file_get_contents($_FILES['filesToUpload']['tmp_name'][$i]));
+            $sql2 = "INSERT INTO image (imgID,img,pdID)VALUES('','$image','$pdID')";//คำสั่งเพิ่มข้อมูล
+            $sql_query2 = mysqli_query($con, $sql2);
+        }
     }
-}
-else{
-    $sql="INSERT INTO product (pdID,name,description,price,cost,dateCreate,color)VALUES('$pdID','$name','$description','$price','$cost','$dateCreate','$color')";//คำสั่งเพิ่มข้อมูล
-    $sql_query=mysqli_query($con,$sql);
-}
+//}
+//else{
+//    $sql="INSERT INTO product (pdID,name,description,price,cost,dateCreate,color)VALUES('$pdID','$name','$description','$price','$cost','$dateCreate','$color')";//คำสั่งเพิ่มข้อมูล
+//    $sql_query=mysqli_query($con,$sql);
+//}
 
 if($sql_query) {
     echo "<script type='text/javascript'>alert('บันทึกข้อมูลเรียบร้อยแล้ว')</script>";
