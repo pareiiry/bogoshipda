b9e871731bd3d2b345ff0ec6269f16331eaba4dc
Pareiiry <pareiiry@gmail.com>
2019-03-07 03:54:15 +0700

    เพิ่มคำแนะนำ

--- member/orderDetail.php
+++ member/orderDetail.php
@@ -1,4 +1,6 @@
 <?php
+//error_reporting(E_ALL ^ E_NOTICE);
+
 session_start();
 if($_SESSION['ID'] == "")
 {
@@ -380,55 +382,63 @@ else{
             $beforeVat = number_format(((str_replace(",", "",$rowOrder['netPrice'])*93)/100),2);
             $vat = number_format(((str_replace(",", "",$rowOrder['netPrice'])*7)/100),2);
 
-            function convert($number){
-                $txtnum1 = array('ศูนย์','หนึ่ง','สอง','สาม','สี่','ห้า','หก','เจ็ด','แปด','เก้า','สิบ');
-                $txtnum2 = array('','สิบ','ร้อย','พัน','หมื่น','แสน','ล้าน','สิบ','ร้อย','พัน','หมื่น','แสน','ล้าน');
-                $number = str_replace(",","",$number);
-                $number = str_replace(" ","",$number);
-                $number = str_replace("บาท","",$number);
-                $number = explode(".",$number);
-                if(sizeof($number)>2){
-                    return 'ทศนิยมหลายตัวนะจ๊ะ';
-                    exit;
-                }
-                $strlen = strlen($number[0]);
-                $convert = '';
-                for($i=0;$i<$strlen;$i++){
-                    $n = substr($number[0], $i,1);
-                    if($n!=0){
-                        if($i==($strlen-1) AND $n==1){ $convert .= 'เอ็ด'; }
-                        elseif($i==($strlen-2) AND $n==2){  $convert .= 'ยี่'; }
-                        elseif($i==($strlen-2) AND $n==1){ $convert .= ''; }
-                        else{ $convert .= $txtnum1[$n]; }
-                        $convert .= $txtnum2[$strlen-$i-1];
-                    }
+            function Convert($amount_number)
+            {
+                $amount_number = number_format($amount_number, 2, ".","");
+                $pt = strpos($amount_number , ".");
+                $number = $fraction = "";
+                if ($pt === false)
+                    $number = $amount_number;
+                else
+                {
+                    $number = substr($amount_number, 0, $pt);
+                    $fraction = substr($amount_number, $pt + 1);
                 }
 
-                $convert .= 'บาท';
-                if($number[1]=='0' OR $number[1]=='00' OR
-                    $number[1]==''){
-                    $convert .= 'ถ้วน';
-                }else{
-                    $strlen = strlen($number[1]);
-                    for($i=0;$i<$strlen;$i++){
-                        $n = substr($number[1], $i,1);
-                        if($n!=0){
-                            if($i==($strlen-1) AND $n==1){$convert
-                                .= 'เอ็ด';}
-                            elseif($i==($strlen-2) AND
-                                $n==2){$convert .= 'ยี่';}
-                            elseif($i==($strlen-2) AND
-                                $n==1){$convert .= '';}
-                            else{ $convert .= $txtnum1[$n];}
-                            $convert .= $txtnum2[$strlen-$i-1];
-                        }
-                    }
-                    $convert .= 'สตางค์';
-                }
-                return $convert;
+                $ret = "";
+                $baht = ReadNumber ($number);
+                if ($baht != "")
+                    $ret .= $baht . "บาท";
+
+                $satang = ReadNumber($fraction);
+                if ($satang != "")
+                    $ret .=  $satang . "สตางค์";
+                else
+                    $ret .= "ถ้วน";
+                return $ret;
+
             }
 
-            $textPrice = convert(str_replace(",", "",$rowOrder['netPrice']));
+            function ReadNumber($number)
+            {
+                $position_call = array("แสน", "หมื่น", "พัน", "ร้อย", "สิบ", "");
+                $number_call = array("", "หนึ่ง", "สอง", "สาม", "สี่", "ห้า", "หก", "เจ็ด", "แปด", "เก้า");
+                $number = $number + 0;
+                $ret = "";
+                if ($number == 0) return $ret;
+                if ($number > 1000000)
+                {
+                    $ret .= ReadNumber(intval($number / 1000000)) . "ล้าน";
+                    $number = intval(fmod($number, 1000000));
+                }
+
+                $divider = 100000;
+                $pos = 0;
+                while($number > 0)
+                {
+                    $d = intval($number / $divider);
+                    $ret .= (($divider == 10) && ($d == 2)) ? "ยี่" :
+                        ((($divider == 10) && ($d == 1)) ? "" :
+                            ((($divider == 1) && ($d == 1) && ($ret != "")) ? "เอ็ด" : $number_call[$d]));
+                    $ret .= ($d ? $position_call[$pos] : "");
+                    $number = $number % $divider;
+                    $divider = $divider / 10;
+                    $pos++;
+                }
+                return $ret;
+            }
+
+            $textPrice = Convert(str_replace(",", "",$rowOrder['netPrice']));
 
             echo "
         <form action=\"../invoice/invoice.php\" method=\"post\" style='text-align: right;margin-right: 8%'>
